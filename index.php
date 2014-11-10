5ac5bd70dde948e4950a5927cf5f8f38cfd6f74b
alibro916 <alibro916@E29603.edu.linkoping.se>
2014-11-10 11:00:17 +0100

    påväg

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+define("DB_SERVER", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_NAME", "login");
+
+$dbh = new PDO('mysql:dbname=' . DB_NAME . ';host=' . DB_SERVER . ';charset=utf8', DB_USER, DB_PASSWORD);
+
+session_start();
+
+if(!isset($_SESSION["login"])){
+    $_SESSION["login"] = null;
+}
+$_SESSION["login"] = filter_input(INPUT_POST, 'anvnam', FILTER_SANITIZE_SPECIAL_CHARS);
+
+//var_dump($_SESSION);
+
+if(!isset($_POST["anvnam"]) and !isset($_POST["losord"])){
+    echo 'Fyll i båda användarnamn och lösenord tack!';   
+}
+$anvnam = filter_input(INPUT_POST, 'anvnam', FILTER_SANITIZE_SPECIAL_CHARS);
+$losord = filter_input(INPUT_POST, 'losord', FILTER_SANITIZE_SPECIAL_CHARS);
+$sql = "SELECT * FROM `login` WHERE username='$anvnam' AND password='$losord'";
+//echo $sql;
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+
+    $login = $stmt->fetch();
+
+
+
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Välkommen, logga in.</title>
+    </head>
+    <body>
+        <form method="POST">
+            <p>Användarnamn:</p> <input type="text" name="anvnam">
+            <p>Lösenord:</p><input type="text" name="losord">
+            <input type="submit" value="Logga in">
+        </form>
+    </body>
+</html>
